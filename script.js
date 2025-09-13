a33f0744dfddedc6c6ca1711bd4e85387cfbce34
Jay <134275522+JayProjectsC@users.noreply.github.com>
2025-09-13 11:12:21 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Auto-redirect to Rickroll video after 10 seconds for troll effect
+    setTimeout(() => {
+        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }, 10000);
+});
